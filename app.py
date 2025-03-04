e1d6ecfceccc5932e231a23530796d4ca6a97427
arleyfernandotorresgalindo <fatorresg@gmail.com>
2025-03-04 19:01:48 +0000

    ok

--- app.py
+++ app.py
@@ -2,22 +2,52 @@ import streamlit as st
 import pandas as pd
 import requests
 from io import BytesIO
-
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import os
 
 EXCEL_URL = 'output.xlsx'
 
+def read_all_sheets_as_dataframes(service_account_file, spreadsheet_id):
+    """Reads all sheets from a Google Spreadsheet and returns them as a dictionary of pandas DataFrames."""
 
-@st.cache_data
-def load_data(url):
     try:
-        df1 = pd.read_excel(url, sheet_name=None, engine="openpyxl")
-        return df1
+        creds = service_account.Credentials.from_service_account_file(
+            service_account_file, scopes=['https://www.googleapis.com/auth/spreadsheets.readonly']
+        )
+        service = build('sheets', 'v4', credentials=creds)
+        sheet_metadata = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+        sheets = sheet_metadata.get('sheets', [])
+
+        all_dfs = {}  # Dictionary to store DataFrames for each sheet
+
+        for sheet in sheets:
+            sheet_name = sheet['properties']['title']
+            range_name = sheet_name  # Read the entire sheet
+
+            result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+            values = result.get('values', [])
+
+            if not values:
+                print(f'No data found in sheet: {sheet_name}')
+                all_dfs[sheet_name] = None  # Store None to indicate an empty sheet
+                continue
+
+            header = values[0]
+            data = values[1:]
+            df = pd.DataFrame(data, columns=header)
+            all_dfs[sheet_name] = df
+
+        return all_dfs
+
     except Exception as e:
-        st.error(f"Error loading data: {e}")
+        print(f"An error occurred: {e}")
         return None
 
-# Load data
-df1 = load_data(EXCEL_URL)
+SERVICE_ACCOUNT_FILE = os.environ.get("GOOGLE_SHEETS_CREDENTIALS")
+SPREADSHEET_ID = '1ypOrL9AQPhFCpEUF9WFGltFwFB6lhKxeqzdYKdPOjwo'
+
+all_dataframes = read_all_sheets_as_dataframes(SERVICE_ACCOUNT_FILE, SPREADSHEET_ID)
 
 st.title("Consulta De Calificaciones 2025-01")
 
@@ -25,7 +55,7 @@ st.title("Consulta De Calificaciones 2025-01")
 materia = st.selectbox('Elegir su materia', ['Precálculo', 'Programación 2', 'Estructura de datos'])
 email = st.text_input("Ingrese su correo electrónico:")
 student_id = st.text_input("Ingrese su número de documento:")
-df = df1[materia]
+df = all_dataframes[materia]
 
 if st.button("Consultar"):
     if not email or not student_id:
